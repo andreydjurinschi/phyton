8e1775820855b8543f7594b9e4c858155d7badd1
andreydjurinschi <djurinschi.andreiai@gmail.com>
2024-04-10 00:36:37 +0300

    10 min to sleeeep

--- phyton5/functons.py
+++ phyton5/functons.py
@@ -1,14 +1,15 @@
 import re
 def inputInfo():
     while True:
+        name = input('Введите имя сотрудника: ')  
+        if not re.match(r'[a-Az-Z]+(?:-[a-Az-Z]*$)'):
+            raise ValueError('Введите фамилию сотрудника заново...')
+            continue  
         surname = input('Введите фамилию сотрудника: ')
         if not re.match(r'[a-Az-Z]+(?:-[a-Az-Z]*$)'):
             raise ValueError('Введите фамилию сотрудника заново...')
             continue
-        name = input('Введите имя сотрудника: ')  
-        if not re.match(r'[a-Az-Z]+(?:-[a-Az-Z]*$)'):
-            raise ValueError('Введите фамилию сотрудника заново...')
-            continue
+
         branch = input('Введите отдел, в котором работает сотрудник: ')
         if not re.match(r'[a-Az-Z0-9]+$)'):
             raise ValueError('Введите отдел сотрудника заново...')
@@ -20,5 +21,12 @@ def inputInfo():
         with open('info.txt', 'a') as file:
             file.write('{}\t{}\t{}\t{}\n'.format(name, surname, branch, kids))
 
-            
+def showInfo():
+    totalKids = 0
+    with open('info.txt', 'r') as file:
+        for employer_info in file:
+            name,surname,branch,kids = employer_info.strip().split('\t')
+            print('Информация о сотрудниках: Имя: {} Фамилия: {} Отдел {} Кол-во детей{}'.format(name, surname, branch, kids))        
+            totalKids += int(kids)
+                     
     
