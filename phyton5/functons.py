96d9172684bbe48ba423ab1909888418e1c83d3e
andreydjurinschi <djurinschi.andreiai@gmail.com>
2024-04-10 10:48:25 +0300

    10:50

--- phyton5/functons.py
+++ phyton5/functons.py
@@ -25,8 +25,38 @@ def showInfo():
     totalKids = 0
     with open('info.txt', 'r') as file:
         for employer_info in file:
-            name,surname,branch,kids = employer_info.strip().split('\t')
+            part = employer_info.strip().split('\t')
+            name = part[0]
+            surname = part[1]
+            branch = part[2]
+            kids = part[3]
             print('Информация о сотрудниках: Имя: {} Фамилия: {} Отдел {} Кол-во детей{}'.format(name, surname, branch, kids))        
             totalKids += int(kids)
-                     
+        print('Всего детей у сотрудников - {}'.format(totalKids))
+        return totalKids
+
+def noChild():
+    childness_array = []
+    with open('info.txt', 'r') as file:
+        for employer_info in file:
+            part = employer_info.strip().split('\t')
+            name = part[0]
+            surname = part[1]
+            kids = part[3]
+            if(kids == '0'):
+                childness_array.append((name,surname))
+    if  childness_array:
+        print('Бездетные сотрудники:')
+        print(childness_array)
+    else:
+        print('Бездетных сотрудников нет')
+
+def main():
+    while True:
+        print('Меню:')
+        print('1) Введите информацию о сотруднике:')
+        print('2) Вывести информацию о сотрудниках и общем кол-ве всех детей:')
+        print('3) Вывести сотрудников у которых нет детей:')
+        print('4) Выход...')
+        answer = input 
     
